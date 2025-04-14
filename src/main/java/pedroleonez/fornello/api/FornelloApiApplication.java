138c0e6d4b0e0c06540b3fe2189714c9f582dc1f
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-14 15:53:17 -0300

    first commit

---
+++ src/main/java/pedroleonez/fornello/api/FornelloApiApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.fornello.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornelloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/fornello/api/FornelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.fornello.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
