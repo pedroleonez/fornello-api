8ad6aa28206bb567a51f4024031e0d80462e6a73
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-16 02:58:14 -0300

    feat: create and impl CreateOrderItemDto

---
+++ src/main/java/pedroleonez/fornello/api/dtos/input/order/CreateOrderItemDto.java
@@ -0,0 +1,17 @@
+package pedroleonez.fornello.api.dtos.input.order;
+
+import jakarta.validation.constraints.NotNull;
+
+public record CreateOrderItemDto(
+
+        @NotNull(message = "{not.null.message}")
+        Long productId,
+
+        @NotNull(message = "{not.null.message}")
+        Long productVariationId,
+
+        @NotNull(message = "{not.null.message}")
+        Integer quantity
+
+) {
+}
