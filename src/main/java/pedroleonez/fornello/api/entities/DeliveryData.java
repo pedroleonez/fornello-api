52b3254dfe2ad58598c49a140b00eb8b2b909e1b
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-16 02:32:52 -0300

    feat: create and impl DeliveryData

---
+++ src/main/java/pedroleonez/fornello/api/entities/DeliveryData.java
@@ -0,0 +1,44 @@
+package pedroleonez.fornello.api.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="deliveries_data")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DeliveryData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name= "order_id")
+    private Order order;
+
+    @Column(name = "receiver_name")
+    private String receiverName;
+
+    private String address;
+
+    @Column(name = "house_number")
+    private String number;
+
+    private String complement;
+
+    private String district;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    private String city;
+
+    private String state;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
