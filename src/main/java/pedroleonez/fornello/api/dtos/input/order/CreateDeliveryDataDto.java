d27e2c06b9c0571e1934dde9727972aa481a4b87
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-16 02:58:42 -0300

    feat: create and impl CreateDeliveryDataDto

---
+++ src/main/java/pedroleonez/fornello/api/dtos/input/order/CreateDeliveryDataDto.java
@@ -0,0 +1,35 @@
+package pedroleonez.fornello.api.dtos.input.order;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateDeliveryDataDto(
+
+        @NotBlank(message = "{not.blank.message}")
+        String receiverName,
+
+        @NotBlank(message = "{not.blank.message}")
+        String address,
+
+        @NotBlank(message = "{not.blank.message}")
+        String number,
+
+        @NotBlank(message = "{not.blank.message}")
+        String complement,
+
+        @NotBlank(message = "{not.blank.message}")
+        String district,
+
+        @NotBlank(message = "{not.blank.message}")
+        String zipCode,
+
+        @NotBlank(message = "{not.blank.message}")
+        String city,
+
+        @NotBlank(message = "{not.blank.message}")
+        String state,
+
+        @NotBlank(message = "{not.blank.message}")
+        String phone_number
+
+) {
+}
